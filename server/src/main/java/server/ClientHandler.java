a5f9fa58e0746a54e530588fe2d30e70aa3d86db
Framzik <framzik@gmail.com>
2021-03-20 20:15:44 +0300

    Добавить на серверную сторону сетевого чата логирование событий (сервер запущен, произошла ошибка, клиент подключился).

--- server/src/main/java/server/ClientHandler.java
+++ server/src/main/java/server/ClientHandler.java
@@ -7,160 +7,171 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
+import java.util.logging.*;
 
 public class ClientHandler {
 
-  private Server server;
-  private Socket socket;
-  private DataInputStream in;
-  private DataOutputStream out;
+    private Server server;
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+    private static Logger logger = Logger.getLogger(ClientHandler.class.getName());
+    ;
+    private String nickname;
+    private String login;
 
-  private String nickname;
-  private String login;
+    private Handler fileHandler;
 
-  public ClientHandler(Server server, Socket socket) {
-    try {
-      this.server = server;
-      this.socket = socket;
-      in = new DataInputStream(socket.getInputStream());
-      out = new DataOutputStream(socket.getOutputStream());
-      /*
-
-        Решил не менять управление потоками через ExecutorService, т.к.
-        1. Я ленивый
-        2. У нас создается всего лишь 1 дополнительный поток(кроме основного), а значит нет смысла использовать
-        ExecutorService, который так же будет создавть 1 поток, и закрывать его.
-
-      */
-      new Thread(() -> {
+    {
         try {
-          // установка сокет тайм аут
-          socket.setSoTimeout(120000);
-
-          // цикл аутентификации
-          while (true) {
-            String str = in.readUTF();
-
-            //если команда отключиться
-            if (str.equals(Command.END)) {
-              out.writeUTF(Command.END);
-              throw new RuntimeException("Клиент захотел отключиться");
-            }
-
-            //если команда аутентификация
-            if (str.startsWith(Command.AUTH)) {
-              String[] token = str.split("\\s", 3);
-              if (token.length < 3) {
-                continue;
-              }
-              String newNick = server.getAuthService()
-                  .getNicknameByLoginAndPassword(token[1], token[2]);
-              login = token[1];
-              if (newNick != null) {
-                if (!server.isLoginAuthenticated(login)) {
-                  nickname = newNick;
-                  sendMsg(Command.AUTH_OK + " " + nickname);
-                  server.subscribe(this);
-                  System.out.println("client: " + socket.getRemoteSocketAddress() +
-                      " connected with nick: " + nickname);
-                  socket.setSoTimeout(0);
-                  break;
-                } else {
-                  sendMsg("Данная учетная запись уже используется");
-                }
-              } else {
-                sendMsg("Неверный логин / пароль");
-              }
-            }
-
-            //если команда регистрация
-            if (str.startsWith(Command.REG)) {
-              String[] token = str.split("\\s", 4);
-              if (token.length < 4) {
-                continue;
-              }
-              boolean regSuccess = server.getAuthService()
-                  .registration(token[1], token[2], token[3]);
-              if (regSuccess) {
-                sendMsg(Command.REG_OK);
-              } else {
-                sendMsg(Command.REG_NO);
-              }
-            }
-          }
-          //цикл работы
-          while (true) {
-            String str = in.readUTF();
-
-            if (str.startsWith("/")) {
-              if (str.equals(Command.END)) {
-                out.writeUTF(Command.END);
-                break;
-              }
-
-              if (str.startsWith(Command.PRIVATE_MSG)) {
-                String[] token = str.split("\\s", 3);
-                if (token.length < 3) {
-                  continue;
-                }
-                server.privateMsg(this, token[1], token[2]);
-              }
-
-              //если команда обновить никнейм
-              if (str.startsWith(Command.UPD_NICKNAME)) {
-                String[] token = str.split("\\s", 2);
-                if (token.length < 2) {
-                  continue;
-                }
-                boolean updNickNameSuccess = server.getAuthService().updNicName(token[1], login);
-                if (updNickNameSuccess) {
-                  server.unsubscribe(this);
-                  nickname = token[1];
-                  server.subscribe(this);
-                  sendMsg(Command.UPD_OK);
-                } else {
-                  sendMsg(Command.UPD_NO);
-                }
-              }
-            } else {
-              server.broadcastMsg(this, str);
-            }
-          }
-          //SocketTimeoutException
-        } catch (SocketTimeoutException e) {
-          this.sendMsg(Command.END);
-        } catch (RuntimeException e) {
-          System.out.println(e.getMessage());
+            fileHandler = new FileHandler("log_" + ClientHandler.class.getName() + "%g.log", true);
+            fileHandler.setFormatter(new SimpleFormatter());
+            logger.addHandler(fileHandler);
         } catch (IOException e) {
-          e.printStackTrace();
-        } finally {
-          server.unsubscribe(this);
-          System.out.println("Client disconnected: " + nickname);
-          try {
-            socket.close();
-          } catch (IOException e) {
             e.printStackTrace();
-          }
         }
-      }).start();
-    } catch (IOException e) {
-      e.printStackTrace();
     }
-  }
 
-  public void sendMsg(String msg) {
-    try {
-      out.writeUTF(msg);
-    } catch (IOException e) {
-      e.printStackTrace();
+    public ClientHandler(Server server, Socket socket) {
+        try {
+            this.server = server;
+            this.socket = socket;
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+
+            new Thread(() -> {
+                try {
+                    // установка сокет тайм аут
+                    socket.setSoTimeout(120000);
+
+                    // цикл аутентификации
+                    while (true) {
+                        String str = in.readUTF();
+
+                        //если команда отключиться
+                        if (str.equals(Command.END)) {
+                            out.writeUTF(Command.END);
+                            logger.log(Level.SEVERE, "Клиент захотел отключиться");
+//              throw new RuntimeException("Клиент захотел отключиться");
+                        }
+
+                        //если команда аутентификация
+                        if (str.startsWith(Command.AUTH)) {
+                            String[] token = str.split("\\s", 3);
+                            if (token.length < 3) {
+                                continue;
+                            }
+                            String newNick = server.getAuthService()
+                                    .getNicknameByLoginAndPassword(token[1], token[2]);
+                            login = token[1];
+                            if (newNick != null) {
+                                if (!server.isLoginAuthenticated(login)) {
+                                    nickname = newNick;
+                                    sendMsg(Command.AUTH_OK + " " + nickname);
+                                    server.subscribe(this);
+                                    logger.log(Level.INFO, "client: " + socket.getRemoteSocketAddress() +
+                                            " connected with nick: " + nickname);
+//                  System.out.println("client: " + socket.getRemoteSocketAddress() +
+//                      " connected with nick: " + nickname);
+                                    socket.setSoTimeout(0);
+                                    break;
+                                } else {
+                                    sendMsg("Данная учетная запись уже используется");
+                                }
+                            } else {
+                                sendMsg("Неверный логин / пароль");
+                            }
+                        }
+
+                        //если команда регистрация
+                        if (str.startsWith(Command.REG)) {
+                            String[] token = str.split("\\s", 4);
+                            if (token.length < 4) {
+                                continue;
+                            }
+                            boolean regSuccess = server.getAuthService()
+                                    .registration(token[1], token[2], token[3]);
+                            if (regSuccess) {
+                                sendMsg(Command.REG_OK);
+                            } else {
+                                sendMsg(Command.REG_NO);
+                            }
+                        }
+                    }
+                    //цикл работы
+                    while (true) {
+                        String str = in.readUTF();
+
+                        if (str.startsWith("/")) {
+                            if (str.equals(Command.END)) {
+                                out.writeUTF(Command.END);
+                                break;
+                            }
+
+                            if (str.startsWith(Command.PRIVATE_MSG)) {
+                                String[] token = str.split("\\s", 3);
+                                if (token.length < 3) {
+                                    continue;
+                                }
+                                server.privateMsg(this, token[1], token[2]);
+                            }
+
+                            //если команда обновить никнейм
+                            if (str.startsWith(Command.UPD_NICKNAME)) {
+                                String[] token = str.split("\\s", 2);
+                                if (token.length < 2) {
+                                    continue;
+                                }
+                                boolean updNickNameSuccess = server.getAuthService().updNicName(token[1], login);
+                                if (updNickNameSuccess) {
+                                    server.unsubscribe(this);
+                                    nickname = token[1];
+                                    server.subscribe(this);
+                                    sendMsg(Command.UPD_OK);
+                                } else {
+                                    sendMsg(Command.UPD_NO);
+                                }
+                            }
+                        } else {
+                            server.broadcastMsg(this, str);
+                        }
+                    }
+                    //SocketTimeoutException
+                } catch (SocketTimeoutException e) {
+                    this.sendMsg(Command.END);
+                } catch (RuntimeException e) {
+                    System.out.println(e.getMessage());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    server.unsubscribe(this);
+                    logger.log(Level.INFO, "Client disconnected: " + nickname);
+//          System.out.println("Client disconnected: " + nickname);
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-  }
 
-  public String getNickname() {
-    return nickname;
-  }
+    public void sendMsg(String msg) {
+        try {
+            out.writeUTF(msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-  public String getLogin() {
-    return login;
-  }
+    public String getNickname() {
+        return nickname;
+    }
+
+    public String getLogin() {
+        return login;
+    }
 }
--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -9,110 +9,122 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.logging.*;
 
 public class Server {
 
-  private final int PORT = 8189;
-  private ServerSocket server;
-  private Socket socket;
-  private DataInputStream in;
-  private DataOutputStream out;
+    private final int PORT = 8189;
+    private ServerSocket server;
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+    private static Logger logger = Logger.getLogger(Server.class.getName());
+    ;
+    private Handler fileHandler;
 
-  private List<ClientHandler> clients;
-  private AuthService authService;
-
-  public Server() {
-    clients = new CopyOnWriteArrayList<>();
-    authService = new DbAuthService();
-
-    try {
-      server = new ServerSocket(PORT);
-      System.out.println("Server started");
-
-      while (true) {
-        socket = server.accept();
-        System.out.println("Client connected");
-        System.out.println("client: " + socket.getRemoteSocketAddress());
-        new ClientHandler(this, socket);
-      }
-
-    } catch (IOException e) {
-      e.printStackTrace();
-    } finally {
-      authService.disconnect();
-      try {
-        socket.close();
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
-      try {
-        server.close();
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
-    }
-  }
-
-  public void broadcastMsg(ClientHandler sender, String msg) {
-    String message = String.format("[ %s ]: %s", sender.getNickname(), msg);
-    for (ClientHandler c : clients) {
-      c.sendMsg(message);
-    }
-  }
-
-  public void privateMsg(ClientHandler sender, String receiver, String msg) {
-    String message = String.format("[ %s ] to [ %s ]: %s", sender.getNickname(), receiver, msg);
-    for (ClientHandler c : clients) {
-      if (c.getNickname().equals(receiver)) {
-        c.sendMsg(message);
-        if (!c.equals(sender)) {
-          sender.sendMsg(message);
+    {
+        try {
+            fileHandler = new FileHandler("log_" + Server.class.getName() + "%g.log", true);
+            fileHandler.setFormatter(new SimpleFormatter());
+            logger.addHandler(fileHandler);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        return;
-      }
-    }
-    sender.sendMsg("not found user: " + receiver);
-  }
-
-  public void subscribe(ClientHandler clientHandler) {
-    clients.add(clientHandler);
-    broadcastClientList();
-  }
-
-  public void unsubscribe(ClientHandler clientHandler) {
-    clients.remove(clientHandler);
-    broadcastClientList();
-  }
-
-  public void updateNickName(String nickname){
-
-  }
-
-
-  public AuthService getAuthService() {
-    return authService;
-  }
-
-  public boolean isLoginAuthenticated(String login) {
-    for (ClientHandler c : clients) {
-      if (c.getLogin().equals(login)) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  public void broadcastClientList() {
-    StringBuilder sb = new StringBuilder(Command.CLIENT_LIST);
-
-    for (ClientHandler c : clients) {
-      sb.append(" ").append(c.getNickname());
     }
 
-    String msg = sb.toString();
+    private List<ClientHandler> clients;
+    private AuthService authService;
 
-    for (ClientHandler c : clients) {
-      c.sendMsg(msg);
+    public Server() {
+        clients = new CopyOnWriteArrayList<>();
+        authService = new DbAuthService();
+
+        try {
+            server = new ServerSocket(PORT);
+            logger.log(Level.INFO, "Server started");
+//      System.out.println("Server started");
+
+            while (true) {
+                socket = server.accept();
+                logger.log(Level.INFO, "Client connected");
+                logger.log(Level.INFO, "client: " + socket.getRemoteSocketAddress());
+//        System.out.println("Client connected");
+//        System.out.println("client: " + socket.getRemoteSocketAddress());
+                new ClientHandler(this, socket);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            authService.disconnect();
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                server.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
-  }
+
+    public void broadcastMsg(ClientHandler sender, String msg) {
+        String message = String.format("[ %s ]: %s", sender.getNickname(), msg);
+        for (ClientHandler c : clients) {
+            c.sendMsg(message);
+        }
+    }
+
+    public void privateMsg(ClientHandler sender, String receiver, String msg) {
+        String message = String.format("[ %s ] to [ %s ]: %s", sender.getNickname(), receiver, msg);
+        for (ClientHandler c : clients) {
+            if (c.getNickname().equals(receiver)) {
+                c.sendMsg(message);
+                if (!c.equals(sender)) {
+                    sender.sendMsg(message);
+                }
+                return;
+            }
+        }
+        sender.sendMsg("not found user: " + receiver);
+    }
+
+    public void subscribe(ClientHandler clientHandler) {
+        clients.add(clientHandler);
+        broadcastClientList();
+    }
+
+    public void unsubscribe(ClientHandler clientHandler) {
+        clients.remove(clientHandler);
+        broadcastClientList();
+    }
+
+    public AuthService getAuthService() {
+        return authService;
+    }
+
+    public boolean isLoginAuthenticated(String login) {
+        for (ClientHandler c : clients) {
+            if (c.getLogin().equals(login)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void broadcastClientList() {
+        StringBuilder sb = new StringBuilder(Command.CLIENT_LIST);
+
+        for (ClientHandler c : clients) {
+            sb.append(" ").append(c.getNickname());
+        }
+
+        String msg = sb.toString();
+
+        for (ClientHandler c : clients) {
+            c.sendMsg(msg);
+        }
+    }
 }
